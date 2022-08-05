3f72c36d465379de6c2351ee4f3c06d105070da5
zbw0046 <zbw0046@gmail.com>
2022-08-05 18:12:42 +0800

    support gofgit

--- install.sh
+++ install.sh
@@ -4,14 +4,18 @@ SHELL_DIR=$(cd "$(dirname "$0")";pwd)
 DIR_NAME=${SHELL_DIR##*/}
 INSTALL_DIR=$1
 if [[ "X$INSTALL_DIR" == "X" ]]; then
-  echo "usage: ./install.sh [TARGET_PATH]"
-  exit -1
+  INSTALL_DIR=/usr/local/share/gof
 fi
+set -x
 
+echo "install in $INSTALL_DIR"
+rm -rf $INSTALL_DIR
 mkdir -p $INSTALL_DIR
 cp -r $SHELL_DIR $INSTALL_DIR
 rm $INSTALL_DIR/$DIR_NAME/install.sh
 
 rm /usr/local/bin/gof
 ln -s $INSTALL_DIR/$DIR_NAME/format_go /usr/local/bin/gof
+sed 's/${GOF_BIN}/gof/g' $INSTALL_DIR/$DIR_NAME/format_go_git > /usr/local/bin/gofgit
+chmod u+x /usr/local/bin/gofgit
 echo "install finish! try" '`gof main.go`'
